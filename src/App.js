c8626fd8e718263b4dcd770aebf33d22683be7a3
Oscar Mesh <okerekeifeanyi65@gmail.com>
2022-07-14 16:12:33 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
-  const { usersList, setUsersList } = useState([]);
+  const [usersList, setUsersList] = useState([]);
 
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, {name: uName, age: uAge}]
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
 
@@ -21,4 +24,3 @@ function App() {
 }
 
 export default App;
- 
